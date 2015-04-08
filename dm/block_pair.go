a07939be53439d678fa196d539e8878c603ba108
James Synge <jamessynge@gmail.com>
2015-04-08 08:41:37 -0400

    Progress on generating BlockPairs from BlockMatches.

--- dm/block_pair.go
+++ dm/block_pair.go
@@ -2,32 +2,7 @@ package dm
 
 import (
 	"log"
-	"sort"
 )
-
-// BlockPairByAIndex implements sort.Interface for []BlockPair based on
-// the AIndex field, then BIndex.
-type BlockPairByAIndex []BlockPair
-
-func (a BlockPairByAIndex) Len() int           { return len(a) }
-func (a BlockPairByAIndex) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a BlockPairByAIndex) Less(i, j int) bool {
-  if a[i].AIndex != a[j].AIndex {
-    return a[i].AIndex < a[j].AIndex
-  }
-  return a[i].BIndex < a[j].BIndex
-}
-
-func SortBlockPairsByAIndex(a []BlockPair) {
-  sort.Sort(BlockPairByAIndex(a))
-}
-
-func SwapBlockPairs(a []BlockPair) {
-  for n := range a {
-    a[n].AIndex, a[n].BIndex = a[n].BIndex, a[n].AIndex
-    a[n].ALength, a[n].BLength = a[n].BLength, a[n].ALength
-  }
-}
 
 // Confused routine to create the blocks that we'll output, representing
 // matches between files, inserted lines, dropped lines, and conflicts.
@@ -37,124 +12,238 @@ func SwapBlockPairs(a []BlockPair) {
 // exactly once and in order, and the other will be secondary, and its lines
 // will appear out of order IFF there have been moves.
 func BlockMatchesToBlockPairs(
-    matches []BlockMatch, aIsPrimary bool, aLineCount, bLineCount int) (
-		pairs []BlockPair) {
-		log.Printf("BlockMatchesToBlockPairs")
-  // Produce BlockPairs indicating a match for each BlockMatch.
-  var rawPairs []BlockPair
-  matchLinesCount := 0
-  for n := range matches {
-    matchLinesCount += matches[n].Length
-    rawPairs = append(pairs, BlockPair{
-        AIndex: matches[n].AIndex,
-        ALength: matches[n].Length,
-        BIndex: matches[n].BIndex,
-        BLength: matches[n].Length,
-        IsMatch: true,
-    })
-  }
+	matches []BlockMatch, aIsPrimary bool, aLineCount, bLineCount int) (
+	pairs []BlockPair) {
+	log.Printf("BlockMatchesToBlockPairs")
+	matchesByA := append([]BlockMatch(nil), matches...)
+	// To simplify the loop below, add a sentinal: an empty match that is at
+	// the end of the file.
+	// TODO Determine if the sentinal helps.
+	// TODO Determine if adding another sentinal to match the start
+	// (i.e. BlockMatch{-1, -1, 0}) would help as well.
+	matchesByA = append(matchesByA, BlockMatch{
+		AIndex: aLineCount,
+		BIndex: bLineCount,
+		Length: 0,
+	})
 
-  // Reduce complexity by treating A as primary by swapping if necessary.
-  if !aIsPrimary {
-    SwapBlockPairs(rawPairs)
-    aLineCount, bLineCount = bLineCount, aLineCount
-  }
-	SortBlockPairsByAIndex(rawPairs)
+	// Reduce complexity by treating A as primary, achieved by swapping if
+	// A is not primary, and swapping again before returning.
+	if !aIsPrimary {
+		SwapBlockMatches(matchesByA)
+		aLineCount, bLineCount = bLineCount, aLineCount
+	}
+	SortBlockMatchesByAIndex(matchesByA)
+	matchesByB := append([]BlockMatch(nil), matchesByA...)
+	SortBlockMatchesByBIndex(matchesByB)
 
-  // Which pairs represent moves, relative to the primary index? Those where
-  // the secondary index isn't ascending.
-  // TODO Not sure how to handle a huge move (such as first line to last line);
-  // want to consider that a move of a single line, rather than as a move of
-  // all the other lines.
-  lowestBIndexAbove := make([]int, len(rawPairs))
-  lowestBIndexAbove[len(rawPairs)-1] = bLineCount
-  bLo := bLineCount
-  outOfOrderMatchCount := 0
-  outOfOrderLinesCount := 0
-  for n := len(rawPairs) - 1; n >= 0; n-- {
-    lowestBIndexAbove[n] = bLo
-    if bLo < rawPairs[n].BIndex {
-      outOfOrderMatchCount++
-      outOfOrderLinesCount += rawPairs[n].BLength
-    } else {
-      bLo = rawPairs[n].BIndex
-    }
-  }
+	// Define BlockMatches U and V in matches to be "adjacent matches"
+	// when there exist integers i and j such that:
+	//      matchesByA[i] == U      matchesByA[i + 1] == V
+	//      matchesByB[j] == U      matchesByB[j + 1] == V
+	// We want to identify such adjacent matches because we can then be
+	// confident in emiting a conflict, insertion or deletion between
+	// two, rather than there being a likelihood that the gap between the
+	// two represents a move.
 
-  if outOfOrderMatchCount * 2 < len(rawPairs) && outOfOrderLinesCount * 2 < matchLinesCount {
-    // 
-  
-  
-  }
+	// Create an index from BlockMatch to position in matchesByB, allowing
+	// us to detect adjacency in B when processing matchesByA.
+	matches2BOrder := make(map[BlockMatch]int)
+	for n := range matchesByB {
+		matches2BOrder[matchesByB[n]] = n
+	}
 
+	isAdjacentToPredecessor := func(aOrder, bOrder int) bool {
+		if aOrder == 0 {
+			return bOrder == 0
+		}
+		return matches2BOrder[matchesByA[aOrder - 1]] == bOrder - 1
+	}
 
-  
-/*
-  for range rawPairs {
-    i := len(rawPairs) -n
-    if n == 0 { continue }
-    if rawPairs[n-1].BIndex > rawPairs[n].BIndex {
-      rawPairs[n-1].IsMove = true
-    }
-  }
-*/
+	// March through the BlockMatches
+	// creating BlockPairs representing matches, moves, and mismatches
+	// (insertions, deletions and conflicts).
+	// We don't emit a non-match when a move is encountered (i.e. between
+	// two sequential matches in matchesByA that are not sequential in
+	// matchesByB).
 
-  for n := range rawPairs {
-    if n == 0 { continue }
-    if rawPairs[n-1].BIndex > rawPairs[n].BIndex {
-      rawPairs[n-1].IsMove = true
-    }
-  }
-  
+	loA, loB := 0, 0
+	for aOrder := range matchesByA {
+		ma := matchesByA[aOrder]
+		bOrder := matches2BOrder[ma]
+		isMove := true
+		if isAdjacentToPredecessor(aOrder, bOrder) {
+			isMove = false
+			aGapLength := ma.AIndex - loA
+			bGapLength := ma.BIndex - loB
+			if aGapLength > 0 || bGapLength > 0 {
+				pairs = append(pairs, BlockPair{
+					AIndex:  loA,
+					ALength: aGapLength,
+					BIndex: loB,
+					BLength: bGapLength,
+					IsMatch: false,
+					IsMove: false,  // Not a move relative to its neighbors.
+				})
+			}
+		}
+		// Emit a match.
+		pairs = append(pairs, BlockPair{
+			AIndex: ma.AIndex,
+			ALength: ma.Length,
+			BIndex: ma.BIndex,
+			BLength: ma.Length,
+			IsMatch: true,
+			IsMove: isMove,
+		})
+		loA, loB = ma.AIndex, ma.BIndex
+	}
 
-
-
-  // Sort by the primary index and fill any gaps in pairs, w.r.t. the primary index.
-  aLo := 0
-  var aPairs []BlockPair
-  for n := range rawPairs {
-    if rawPairs[n].AIndex > aLo {
-      // There is a gap in the primary key.  Insert a mismatch.
-      // TODO Need a way to find this entry later when dealing with gaps
-      // in the secondary key.
-      aPairs = append(aPairs,  BlockPair{
-          AIndex: aLo,
-          ALength: rawPairs[n].AIndex - aLo,
-          BIndex: -1,
-          BLength: -1,
-          IsMatch: true,
-      })
-    } 
-  }
-
-
-
-  // Produce BlockPairs indicating a match for each BlockMatch.
-  
+	// Sort the BlockPairs by B, and fill the gaps with Inserts (BlockPairs
+	// with no corresponding lines in A).
+	SortBlockPairsByBIndex(pairs)
+	loB = 0
 
 
 
 
-/*
+		ma, mb := matchesByA[n], matchesByB[n]
+		if ma == mb {
+			// Alignment maintained.
+			// Completely unsure what I want for BIndex in this BlockPair, in
+			// the face of moves. Thought needed.
+			nextB := maxInt(loB, minInt(ma.BIndex, mb.BIndex))
+			if loA < ma.AIndex || loB < nextB {
+				// There is a gap below to be expressed.
+				pairs = append(pairs, BlockPair{
+					AIndex:  loA,
+					ALength: ma.AIndex - loA,
+					BIndex: loB,
+					BLength: nextB - loB,
+					IsMatch: false,
+					IsMove: false,
+				})
+				loA, loB = ma.AIndex, nextB
+			}
+		
+		
+		
+		}
+	
+	
+	
+	}
+	
 
-// Represents a pairing of ranges in files A and B, primarily for output,
-// as we can produce different pairings based on which file we consider
-// primary (i.e. in the face of block moves we may print A in order, but
-// B out of order).
-type BlockPair struct {
-	AIndex, ALength int
-	BIndex, BLength int
-	IsMatch         bool
+
+
+
+
+	// Produce BlockPairs indicating a match for each BlockMatch.
+	var pairsA, pairsB []BlockPair
+	matchLinesCount := 0
+	for n := range matches {
+		matchLinesCount += matches[n].Length
+		pairsA = append(pairsA, BlockPair{
+			AIndex:  matches[n].AIndex,
+			ALength: matches[n].Length,
+			BIndex:  matches[n].BIndex,
+			BLength: matches[n].Length,
+			IsMatch: true,
+		})
+	}
+
+	// Reduce complexity by treating A as primary, achieved by swapping if
+	// A is not primary, and then again before returning.
+	if !aIsPrimary {
+		SwapBlockPairs(pairsA)
+		aLineCount, bLineCount = bLineCount, aLineCount
+	}
+
+	// Use two arrays of pairs, sorted by the two indices.
+	SortBlockPairsByAIndex(pairsA)
+	pairsB := append([]BlockPair(nil), pairsA...)
+
+	// Which pairs represent moves, relative to the primary index? Those where
+	// the secondary index isn't ascending.
+	// TODO Not sure how to handle a huge move (such as first line to last line);
+	// want to consider that a move of a single line, rather than as a move of
+	// all the other lines.
+	lowestBIndexAbove := make([]int, len(rawPairs))
+	lowestBIndexAbove[len(rawPairs)-1] = bLineCount
+	bLo := bLineCount
+	outOfOrderMatchCount := 0
+	outOfOrderLinesCount := 0
+	for n := len(rawPairs) - 1; n >= 0; n-- {
+		lowestBIndexAbove[n] = bLo
+		if bLo < rawPairs[n].BIndex {
+			outOfOrderMatchCount++
+			outOfOrderLinesCount += rawPairs[n].BLength
+		} else {
+			bLo = rawPairs[n].BIndex
+		}
+	}
+
+	if outOfOrderMatchCount*2 < len(rawPairs) && outOfOrderLinesCount*2 < matchLinesCount {
+		//
+
+	}
+
+	/*
+	   for range rawPairs {
+	     i := len(rawPairs) -n
+	     if n == 0 { continue }
+	     if rawPairs[n-1].BIndex > rawPairs[n].BIndex {
+	       rawPairs[n-1].IsMove = true
+	     }
+	   }
+	*/
+
+	for n := range rawPairs {
+		if n == 0 {
+			continue
+		}
+		if rawPairs[n-1].BIndex > rawPairs[n].BIndex {
+			rawPairs[n-1].IsMove = true
+		}
+	}
+
+	// Sort by the primary index and fill any gaps in pairs, w.r.t. the primary index.
+	aLo := 0
+	var aPairs []BlockPair
+	for n := range rawPairs {
+		if rawPairs[n].AIndex > aLo {
+			// There is a gap in the primary key.  Insert a mismatch.
+			// TODO Need a way to find this entry later when dealing with gaps
+			// in the secondary key.
+			aPairs = append(aPairs, BlockPair{
+				AIndex:  aLo,
+				ALength: rawPairs[n].AIndex - aLo,
+				BIndex:  -1,
+				BLength: -1,
+				IsMatch: true,
+			})
+		}
+	}
+
+	// Produce BlockPairs indicating a match for each BlockMatch.
+
+	/*
+
+	   // Represents a pairing of ranges in files A and B, primarily for output,
+	   // as we can produce different pairings based on which file we consider
+	   // primary (i.e. in the face of block moves we may print A in order, but
+	   // B out of order).
+	   type BlockPair struct {
+	   	AIndex, ALength int
+	   	BIndex, BLength int
+	   	IsMatch         bool
+	   }
+	*/
+
+	if !aIsPrimary {
+		SwapBlockPairs(pairs)
+	}
+
+	return
 }
-*/
-
-
-  if !aIsPrimary {
-    SwapBlockPairs(pairs)
-  }
-
-  return
-}
-
-
-
--- dm/utils.go
+++ dm/utils.go
@@ -13,6 +13,14 @@ func minInt(i, j int) int {
 	}
 }
 
+func maxInt(i, j int) int {
+	if i < j {
+		return j
+	} else {
+		return i
+	}
+}
+
 func findLineWithIndex(lines []LinePos, index int) int {
 	for lo, hi := 0, len(lines)-1; lo <= hi; {
 		mid := (lo + hi) / 2
@@ -71,7 +79,6 @@ func selectCommonUniqueLines(aLines, bLines []LinePos, counts map[uint32]int) (
 // BlockMatchByAIndex implements sort.Interface for []BlockMatch based on
 // the AIndex field, then BIndex.
 type BlockMatchByAIndex []BlockMatch
-
 func (a BlockMatchByAIndex) Len() int           { return len(a) }
 func (a BlockMatchByAIndex) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a BlockMatchByAIndex) Less(i, j int) bool {
@@ -80,9 +87,55 @@ func (a BlockMatchByAIndex) Less(i, j int) bool {
   }
   return a[i].BIndex < a[j].BIndex
 }
-
 func SortBlockMatchesByAIndex(a []BlockMatch) {
   sort.Sort(BlockMatchByAIndex(a))
+}
+
+// BlockMatchByBIndex implements sort.Interface for []BlockMatch based on
+// the BIndex field, then AIndex.
+type BlockMatchByBIndex []BlockMatch
+func (a BlockMatchByBIndex) Len() int           { return len(a) }
+func (a BlockMatchByBIndex) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a BlockMatchByBIndex) Less(i, j int) bool {
+  if a[i].BIndex != a[j].BIndex {
+    return a[i].BIndex < a[j].BIndex
+  }
+  return a[i].AIndex < a[j].AIndex
+}
+func SortBlockMatchesByBIndex(a []BlockMatch) {
+  sort.Sort(BlockMatchByBIndex(a))
+}
+
+// BlockPairByAIndex implements sort.Interface for []BlockPair based on
+// the AIndex field, then BIndex.
+type BlockPairByAIndex []BlockPair
+
+func (a BlockPairByAIndex) Len() int           { return len(a) }
+func (a BlockPairByAIndex) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a BlockPairByAIndex) Less(i, j int) bool {
+  if a[i].AIndex != a[j].AIndex {
+    return a[i].AIndex < a[j].AIndex
+  }
+  return a[i].BIndex < a[j].BIndex
+}
+func SortBlockPairsByAIndex(a []BlockPair) {
+  sort.Sort(BlockPairByAIndex(a))
+}
+
+// BlockPairByBIndex implements sort.Interface for []BlockPair based on
+// the BIndex field, then AIndex.
+type BlockPairByBIndex []BlockPair
+
+func (a BlockPairByBIndex) Len() int           { return len(a) }
+func (a BlockPairByBIndex) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a BlockPairByBIndex) Less(i, j int) bool {
+  if a[i].BIndex != a[j].BIndex {
+    return a[i].BIndex < a[j].BIndex
+  }
+  return a[i].AIndex < a[j].AIndex
+}
+func SortBlockPairsByBIndex(a []BlockPair) {
+  sort.Sort(BlockPairByBIndex(a))
 }
 
 func SwapBlockMatches(a []BlockMatch) {
@@ -91,5 +144,12 @@ func SwapBlockMatches(a []BlockMatch) {
   }
 }
 
+
+func SwapBlockPairs(a []BlockPair) {
+  for n := range a {
+    a[n].AIndex, a[n].BIndex = a[n].BIndex, a[n].AIndex
+    a[n].ALength, a[n].BLength = a[n].BLength, a[n].ALength
+  }
+}
 
 
